c84cef60f3b4be0e916fcf12601b1bee55758793
Bartosz <bartosznovak@gmail.com>
2021-03-18 00:03:09 +0100

    Files clean up

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return defaultConfig;
+  }
+
+  return {
+    ...defaultConfig,
+    images: {
+      loader: "imgix",
+      path: "https://noop/",
+    },
+  };
+};
