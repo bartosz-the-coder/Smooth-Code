3e3ba9837d239ef7fbb767995822463b3f75683d
Bartosz <bartosznovak@gmail.com>
2021-03-18 00:30:06 +0100

    Simple content for About and Contact pages

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,13 +1,33 @@
 export default function About(props) {
   return (
-    <div>
-      <h2>My exprestise</h2>
-      <div>
-        My experience
-      </div>
-      <div>
+    <div style={{ padding: "2rem" }}>
+      <h2>My expertise</h2>
+      <section>My experience
+        <ul>
+          <li>Avanade</li>
+          <li>EY GDS Poland</li>
+          <li>Credit Suisse</li>
+          <li>Aduma Mobile</li>
+        </ul>
+      </section>
+      <section>
         My skills
-      </div>
+        <ul>
+          <li>.NET/C#</li>
+          <li>React</li>
+          <li>JavaScript / TypeScript</li>
+          <li>CSS 3</li>
+          <li>HTML 5</li>
+          <li>GIT</li>
+        </ul>
+      </section>
+      <section>
+        Education
+        <ul>
+          <li>Wrocław University of Technology</li>
+          <li>Częstochowa University of Technology</li>
+        </ul>
+      </section>
     </div>
   );
 }
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,8 @@
 export default function Contact(props) {
   return (
-    <div>
+    <div style={{ padding: "2rem" }}>
       <h2>Contact</h2>
+      <h3>contact@smoothcode.pl</h3>
     </div>
   );
 }
