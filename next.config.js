5d3d55a45bd3ddd9af3b08b08820edde43ac39e1
Bartosz <bartosznovak@gmail.com>
2021-03-18 00:30:19 +0100

    Next configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase, { defaultConfig }) => {
+  const config = Object.assign({}, defaultConfig, {
+    trailingSlash: true,
+    exportPathMap: function () {
+      return {
+        "/": { page: "/" },
+      };
+    },
+  });
+
   if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return defaultConfig;
+    return config;
   }
 
   return {
-    ...defaultConfig,
+    ...config,
     images: {
       loader: "imgix",
       path: "https://noop/",
