bf51265f89a23373c478b06df7941d04f279bcc4
Bartosz <bartosznovak@gmail.com>
2021-03-18 02:58:46 +0100

    About page updated

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,18 +1,33 @@
+import styles from "styles/About.module.css";
+
 export default function About(props) {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h2>My expertise</h2>
-      <section>My experience
-        <ul>
-          <li>Avanade</li>
-          <li>EY GDS Poland</li>
-          <li>Credit Suisse</li>
-          <li>Aduma Mobile</li>
+    <div className={styles.container}>
+      <h2>My Expertises</h2>
+      <section>
+        <h3>My&nbsp;experience</h3>
+        <ul className={styles.list}>
+          <li>
+            <div>Avanade</div>
+            <div className={styles.badge}>APR 2021 → CURRENTLY</div>
+          </li>
+          <li>
+            <div>EY GDS Poland</div>
+            <div className={styles.badge}>JUN 2017 → MAR 2021</div>
+          </li>
+          <li>
+            <div>Credit Suisse</div>
+            <div className={styles.badge}>SEP 2013 → MAY 2017</div>
+          </li>
+          <li>
+            <div>Aduma Mobile</div>
+            <div className={styles.badge}>JUL 2013 → AUG 2013</div>
+          </li>
         </ul>
       </section>
       <section>
-        My skills
-        <ul>
+        <h3>My&nbsp;skills</h3>
+        <ul className={styles.list}>
           <li>.NET/C#</li>
           <li>React</li>
           <li>JavaScript / TypeScript</li>
@@ -22,8 +37,8 @@ export default function About(props) {
         </ul>
       </section>
       <section>
-        Education
-        <ul>
+        <h3>Education</h3>
+        <ul className={styles.list}>
           <li>Wroclaw University of Technology</li>
           <li>Czestochowa University of Technology</li>
         </ul>
