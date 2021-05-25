ca001fe89b2a5023e9c57b0b6f6d4875209e9f0c
Bartosz <bartosznovak@gmail.com>
2021-05-25 19:22:56 +0200

    Experience dates fixed. Selection for badges disabled

--- data/experience.js
+++ data/experience.js
@@ -4,7 +4,7 @@ export const experience = [
   {
     title: "Software Engineer / Self-employed",
     employer: "Avanade",
-    startDate: getFormattedDate(getFormattedDate(new Date(2021, 4, 1))),
+    startDate: getFormattedDate(getFormattedDate(new Date(2021, 3, 1))),
     endDate: getFormattedDate(),
     description:
       "Development of multiple React web applications for tax return and settlement.",
@@ -12,24 +12,24 @@ export const experience = [
   {
     title: "Web developer / Technical Leader",
     employer: "EY GDS (CS) Poland",
-    startDate: getFormattedDate(new Date(2017, 6, 1)),
-    endDate: getFormattedDate(new Date(2021, 3, 30)),
+    startDate: getFormattedDate(new Date(2017, 5, 1)),
+    endDate: getFormattedDate(new Date(2021, 2, 30)),
     description:
       "Development of multiple React web applications for tax return and settlement.",
   },
   {
     title: ".NET Software Engineer",
     employer: "Credit Suisse Poland",
-    startDate: getFormattedDate(new Date(2013, 9, 1)),
-    endDate: getFormattedDate(new Date(2017, 5, 30)),
+    startDate: getFormattedDate(new Date(2013, 8, 1)),
+    endDate: getFormattedDate(new Date(2017, 4, 30)),
     description:
       "Development and maintainence of WPF client applications for market risk management.",
   },
   {
     title: "Android Developer",
     employer: "Aduma Mobile",
-    startDate: getFormattedDate(new Date(2013, 6, 1)),
-    endDate: getFormattedDate(new Date(2013, 8, 30)),
+    startDate: getFormattedDate(new Date(2013, 5, 1)),
+    endDate: getFormattedDate(new Date(2013, 7, 30)),
     description:
       "Internship. Development of Android client application for shopping centre.",
   },
