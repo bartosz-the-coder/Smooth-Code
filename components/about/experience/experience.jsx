3da7e0659c59bbd879cce85f6c84a0c8a8f8d753
Bartosz <bartosznovak@gmail.com>
2023-02-04 19:15:40 +0100

    feature(Experience): add job description

--- components/about/experience/experience.jsx
+++ components/about/experience/experience.jsx
@@ -1,7 +1,14 @@
 import { PeriodBadge } from "components/badge";
 import styles from "./styles.module.css";
 
-export const ExperienceRow = ({ employer, title, type, startDate, endDate }) => (
+export const ExperienceRow = ({
+  employer,
+  title,
+  type,
+  startDate,
+  endDate,
+  projectDescription,
+}) => (
   <li className={styles.experience_item}>
     <div className={styles.job_details}>
       <div>
@@ -9,6 +16,20 @@ export const ExperienceRow = ({ employer, title, type, startDate, endDate }) => 
         <span className={styles.job_employer}>{employer}</span>
       </div>
       <div className={styles.job_contractor}>{type}</div>
+      <details className={styles.job_description}>
+        <summary>Job details</summary>
+        <article>
+          <p> {projectDescription}</p>
+          <p>Required skills</p>
+          <ul>
+            <li>Skill 1</li>
+            <li>Skill X</li>
+            <li>Skill X</li>
+            <li>Skill X</li>
+            <li>Skill X</li>
+          </ul>
+        </article>
+      </details>
     </div>
     <PeriodBadge start={startDate} end={endDate} />
   </li>
