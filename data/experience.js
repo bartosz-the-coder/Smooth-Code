fd9d5808c55f1266a046c84f03dd20469f9f8572
Bartosz <bartosznovak@gmail.com>
2023-02-05 17:44:13 +0100

    chore(data): update experience feed

--- data/experience.js
+++ data/experience.js
@@ -3,71 +3,104 @@ import { getFormattedDate } from "./utils";
 export const experience = [
   {
     title: "Senior Frontend Developer",
-    type: 'Contractor',
+    type: "Contractor",
     employer: "Galaxy Digital",
     startDate: getFormattedDate(new Date(2021, 9, 1)),
     endDate: getFormattedDate(),
-    technology: "React",
-    projectDescription: "Development of cryptocurrency trading apps in React",
+    project: {
+      technologies: [
+        { name: 'React', iconName: 'React' }
+      ],
+      description: "Development of cryptocurrency trading apps in React",
+    },
   },
   {
     title: "Senior Frontend Engineer",
-    type: 'Contractor',
+    type: "Contractor",
     employer: "GRID esports",
     startDate: getFormattedDate(new Date(2021, 7, 1)),
     endDate: getFormattedDate(new Date(2022, 4, 30)),
-    technology: "React",
-    projectDescription:
-      "Development an esport data hub in React ",
+    project: {
+      technologies: [
+        { name: 'React', iconName: 'React' }
+      ],
+      description: "Development an esport data hub in React ",
+    },
   },
   {
     title: "Software Engineer",
-    type: 'Contractor',
+    type: "Contractor",
     employer: "Avanade",
     startDate: getFormattedDate(new Date(2021, 3, 1)),
     endDate: getFormattedDate(new Date(2021, 6, 30)),
-    technology: "React",
-    projectDescription:
-      "Development of React web application for content management.",
+    project: {
+      technologies: [
+        { name: 'React', iconName: 'React' }
+      ],
+      description:
+        "Development of React web application for content management.",
+    },
   },
   {
     title: "Technical Leader",
-    type: 'Full time employment',
+    type: "Full time employment",
     employer: "EY GDS (CS) Poland",
     startDate: getFormattedDate(new Date(2019, 9, 1)),
     endDate: getFormattedDate(new Date(2021, 2, 30)),
-    technology: "React",
-    projectDescription:
-      "Leading a development team working on various React web applications for tax return and settlement.",
+    project: {
+      technologies: [
+        { name: 'React', iconName: 'React' }
+      ],
+      description:
+        "Leading a development team working on various React web applications for tax return and settlement.",
+    },
   },
   {
     title: "Senior Web Developer",
-    type: 'Full time employment',
+    type: "Full time employment",
     employer: "EY GDS (CS) Poland",
     startDate: getFormattedDate(new Date(2017, 5, 1)),
     endDate: getFormattedDate(new Date(2029, 8, 30)),
-    technology: "React",
-    projectDescription:
-      "Development of multiple React web applications for tax return and settlement.",
+    project: {
+      technologies: [
+        { name: 'React', iconName: 'React' }
+      ],
+      description:
+        "Development of multiple React web applications for tax return and settlement.",
+    },
   },
   {
     title: ".NET Software Engineer",
-    type: 'Full time employment',
+    type: "Full time employment",
     employer: "Credit Suisse Poland",
     startDate: getFormattedDate(new Date(2013, 8, 1)),
     endDate: getFormattedDate(new Date(2017, 4, 30)),
-    technology: ".NET / WPF",
-    projectDescription:
-      "Development and maintainence of WPF client applications for market risk management.",
+    project: {
+      technologies: [
+        { name: '.NET', iconName: 'Dotnet'}, 
+        { name: 'WPF', iconName: 'Dotnet'}, 
+        { name: 'ASP.NET', iconName: 'Dotnet'}, 
+      ],
+      description:
+        `
+          Development and maintainence of WPF client applications for market risk management.
+          Development of ASP.NET WebAPI service.
+        `,
+    },
   },
   {
     title: "Android Developer",
-    type: 'Full time employment',
+    type: "Full time employment",
     employer: "Aduma Mobile",
     startDate: getFormattedDate(new Date(2013, 5, 1)),
     endDate: getFormattedDate(new Date(2013, 7, 30)),
-    technology: "Android (Java)",
-    projectDescription:
-      "Internship. Development of Android client application for shopping centre.",
+    project: {
+      technologies: [
+        { name: 'Android', iconName: 'Android' },
+        { name: 'Java', iconName: 'Java' }
+      ],
+      description:
+        "Internship. Development of Android client application for shopping centre.",
+    },
   },
 ];
